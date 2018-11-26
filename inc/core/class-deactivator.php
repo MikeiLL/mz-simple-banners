11a065bb1dbef8da3b8fa5570a5cb748f419e8f0
Mike iLL Kilmer <mike@mzoo.org>
2018-11-26 17:58:51 -0600

    Initial commit.

---
+++ inc/core/class-deactivator.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Mz_Simple_Banners\Inc\Core;
+
+/**
+ * Fired during plugin deactivation
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @link       http://mzoo.org
+ * @since      1.0.0
+ *
+ * @author     Lex Web Dev
+ **/
+class Deactivator {
+
+	/**
+	 * Short Description.
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
